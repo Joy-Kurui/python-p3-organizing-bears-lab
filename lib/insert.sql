a69baf1949eca24b481dbaae30d37d5063a1893d
joy Kurui <kurui.chepngetich@gmail.com>
2024-01-04 19:15:43 +0300

    Completed Assignment

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+bears(id, name, age, sex, color, temperament, alive)
+VALUES(1, "Mr. Chocolate", 3, "M", "Brown", "calm", 0),
+(2,"Rowdy", 5, "F", "black", "intense", 1),
+(3,"Tabitha", 4, "F", "light brown", "Nice", 1),
+(4,"Sergeant Brown", 7, "M", "Green", "goofy", 0),
+(5,"Melissa", 12, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 10, "F", "White", "Grinchy", 1),
+(7,"Wendy", 8, "M", "Blue", "naive", 1),
+(8,null, 18, "M", "orange", "calm", 0);
